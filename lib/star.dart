56de6dbe3f50098ec17750c57d9806ae7186492d
arshaajith1 <arshaajith18@gmail.com>
2024-04-24 10:04:14 +0530

    code1

--- lib/star.dart
+++ lib/star.dart
@@ -7,43 +7,12 @@ class Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      backgroundColor: Colors.teal,
-      body: Column(
-        children: [
-          Center(
-            child:
-              CircleAvatar(
-               backgroundImage: NetworkImage("https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTExL3Jhd3BpeGVsX29mZmljZV8yN19hX2dyb3VwX29mX2ZsdWZmeV9wYXN0ZWxfY2F0X2thd2FpaV9hZXN0aGV0aV80OGUxYmFhOS1iYjJlLTRlMmUtYWYwZC02YWQzOWVkYzI0NDlfMS5qcGc.jpg"),
-              radius: 40.0,
-            ),
-              ),
-          Text("SWEET CAT"),
-                Container(
-    height: 30.0,
-    color: Colors.yellow,
-    child:Row(
-    children: [
-      Icon(Icons.call),
-    Text('12345678'),
-    ],
+      appBar: AppBar(
+        foregroundColor: Colors.blue,
+        titleTextStyle: TextStyle(color: Colors.white),
+
       ),
 
-    ),
-          Container(
-            height: 30.0,
-            color: Colors.white,
-            child:Row(
-              children: [
-                Icon(Icons.mail),
-                Text('abc@gmail.com'),
-
-    ],
-      ),
-    ),
-    ],
-      ),
-    ],
-      ),
     );
   }
 }
